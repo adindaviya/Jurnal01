8b8d80dc90f490c47cea69fe0b249326cb4006a7
adindaviya <adindaalifviya@gmail.com>
2023-09-27 09:19:19 +0700

    Kumpul jurnal 01

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+public class Jurnal01 {
+    public static void main (String []args){
+        System.out.println("Nama:Adinda Alif Viya");
+        System.out.println("NIM:607062300052");
+        System.out.println("Kelas:D3IF-47-04");
+    }
+}
